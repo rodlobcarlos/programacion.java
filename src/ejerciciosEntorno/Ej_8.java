08e05ef8aaad625779f705a22f2acc9c7a166e3a
rodlobcarlos <rodlobcarlos@gmail.com>
2025-02-19 08:09:31 +0100

    new changes

--- src/ejerciciosEntorno/Ej_8.java
+++ src/ejerciciosEntorno/Ej_8.java
@@ -4,6 +4,7 @@ public class Ej_8 {
 
 	public static void main(String[] args) {
 		String palabra = null;
+		
 		if (palabra != null) {
 			int longitud = palabra.length();
 			System.out.println("Longitud de la palabra: " + longitud);
